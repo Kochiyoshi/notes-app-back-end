b6629c50e8114e7c25e0b35d586e8531370efd9e
Arief Maulana <arief.thehunter34@gmail.com>
2022-04-19 21:59:23 +0700

    2nd commit - ESlint implementation

--- src/server.js
+++ src/server.js
@@ -1,29 +1,28 @@
-'use strict';
-
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
+  const server = Hapi.server({
+    host: 'localhost',
+    port: 5000,
+    routes: {
+      cors: {
+        origin: ['http://notesapp-v1.dicodingacademy.com'],
+      },
+    },
+  });
 
-    const server = Hapi.server({
-        host: 'localhost',
-        port: 5000,
-        routes: {
-            cors: {
-              origin: ['http://notesapp-v1.dicodingacademy.com'],
-            }
-          }
-    });
+  server.route(routes);
 
-    server.route(routes);
-
-    await server.start();
-    console.log(`server running in ${server.info.uri}`);
-}
+  await server.start();
+  // eslint-disable-next-line no-console
+  console.log(`server running in ${server.info.uri}`);
+};
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  // eslint-disable-next-line no-console
+  console.log(err);
+  process.exit(1);
 });
 
 init();
